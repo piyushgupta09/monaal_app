759549a583efce3d9bd33159b0565a1c4f4ed1bf
Piyush Gupta <piyush@fpaipl.com>
2023-10-20 17:52:22 +0530

    adj for panel

--- config/app.php
+++ config/app.php
@@ -164,7 +164,7 @@ return [
          */
         Fpaipl\Authy\AuthyServiceProvider::class,
         Fpaipl\Panel\PanelServiceProvider::class,
-        Fpaipl\Shopy\ShopyServiceProvider::class,
+        
         
         /*
          * Application Service Providers...
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    return view('panel::pages.welcome');
 });
